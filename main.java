590d33dcc200f51a86e60dccead28ed0e571d253
Wilson Rangari <98660886+Wilsonrangari@users.noreply.github.com>
2024-07-18 12:40:52 +0200

    Added one more file

--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+package JavaApp_Jenkins;
 import java.util.Date;
 
 public class Main{
@@ -8,6 +9,7 @@ public static void main(String[] args){
     System.out.println("Hi there");
     System.out.println(new Date());
     System.out.println("Hello");
+    System.out.println("Added on emore file");
 
 }
 
