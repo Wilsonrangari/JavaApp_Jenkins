396bdf4ac345875983a4d76101939109ae192c9f
Wilson Rangari <98660886+Wilsonrangari@users.noreply.github.com>
2024-07-18 12:36:44 +0200

    Initial commit

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Main{
+
+public static void main(String[] args){
+
+    System.out.println("Hello World");
+    System.out.println("Hi there");
+    System.out.println(new Date());
+    System.out.println("Hello");
+
+}
+
+}
